9d0d2d297302b61ee6d12ea4fc71e8fdde1ed34f
Valerii Strilets <valerii.strilets@gmail.com>
2026-03-06 02:15:18 +0200

    chore: add comment

--- seitu/vitest.config.ts
+++ seitu/vitest.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'happy-dom',
+    // https://github.com/capricorn86/happy-dom/issues/1950#issuecomment-3523878228
     execArgv: [
       '--localstorage-file',
       path.resolve(os.tmpdir(), `vitest-${process.pid}.localstorage`),
