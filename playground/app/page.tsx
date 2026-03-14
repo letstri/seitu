7b6c3325f2e82c248920cccfce7619fb67ca32d9
Valerii Strilets <valerii.strilets@gmail.com>
2026-03-14 18:18:33 +0200

    chore: update playground

--- playground/app/page.tsx
+++ playground/app/page.tsx
@@ -1,59 +1,44 @@
 'use client'
 
-import { useRef } from 'react'
-import { useSubscription } from 'seitu/react'
+import { createComputed } from 'seitu'
+import { Subscription, useSubscription } from 'seitu/react'
 import { createLocalStorage } from 'seitu/web'
 import * as z from 'zod'
 
 const localStorage = createLocalStorage({
   schemas: {
-    count: z.number(),
+    firstName: z.string(),
+    lastName: z.string(),
   },
   defaultValues: {
-    count: 0,
+    firstName: 'John',
+    lastName: 'Doe',
   },
 })
 
-function CountRender() {
-  const renderCount = useRef(1).current++
-  const count = useSubscription(localStorage, { selector: value => value.count })
+const fullName = createComputed(localStorage, s => `${s.firstName} ${s.lastName}`)
+
+export default function Page() {
+  const firstName = useSubscription(localStorage, { selector: s => s.firstName })
+  const name = useSubscription(fullName)
+
   return (
     <>
-      <p>
-        Count:
-        {count}
-      </p>
-      <p style={{ opacity: 0.5 }}>
-        Render count:
-        {renderCount}
-      </p>
+      <input
+        type="text"
+        value={firstName}
+        onChange={e => localStorage.set({ firstName: e.target.value })}
+      />
+      <Subscription value={localStorage} selector={s => s.lastName}>
+        {lastName => (
+          <input
+            type="text"
+            value={lastName}
+            onChange={e => localStorage.set({ lastName: e.target.value })}
+          />
+        )}
+      </Subscription>
+      <span>{name}</span>
     </>
-  )
-}
-
-function CountMod10() {
-  const renderCount = useRef(1).current++
-  const mod10 = useSubscription(localStorage, { selector: value => value.count % 10 === 0 })
-  return (
-    <>
-      <p>
-        Mod 10:
-        {mod10 ? 'Yes' : 'No'}
-      </p>
-      <p style={{ opacity: 0.5 }}>
-        Render count:
-        {renderCount}
-      </p>
-    </>
-  )
-}
-
-export default function Home() {
-  return (
-    <div>
-      <button onClick={() => localStorage.set(({ count }) => ({ count: count + 1 }))}>Increment</button>
-      <CountRender />
-      <CountMod10 />
-    </div>
   )
 }
